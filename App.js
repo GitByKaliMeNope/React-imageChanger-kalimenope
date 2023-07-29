b74ff0926338f4574d6493c42f7e8eddf3bd28b3
Timsen@KaliMeNope.de <136541891+GitByKaliMeNope@users.noreply.github.com>
2023-07-29 20:40:04 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -1,15 +1,54 @@
 import './App.css';
-import ImageChanger from './imageChanger';
+import ImageChanger from './components/imageChanger';
+import PropTypes  from 'prop-types';
+
+const Images = [
+  {   
+      
+      src:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.CO-LNiUayQer2VfMfYvoFAHaEf%26pid%3DApi&f=1&ipt=9e5b709427c50c94e6b3adfcdf82199861284099ebcb6b83b8b93d349f744644&ipo=images',
+      alt:'fuckYou'
+  },
+  {
+     
+      src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1yLWlhkn4jj3xufMaHayS7keZ0Ary2KfNmA&usqp=CAU',
+      alt:'fuckYou2'
+  },
+  {
+    
+      src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6y_fltB6Mn2NtTbdRcmphZg-h_TTDUZqrGA&usqp=CAU',
+      alt:'fuckYou3'
+  }
+]
+const Strings = [
+  {
+      String: ['Welcome to']
+  },{
+      String: ['KaliMeNope.de']
+  },{
+      String: ['Website about development']
+  },{
+      String: ['and nice Stuff like Hacker-Gadgets']
+  }
+]
+
+
 
 function App() {
+ 
+  
   return (
     <div className="App">
       <header className="App-header">
-        <ImageChanger />
+        <ImageChanger Images={Images} Strings={Strings} interval={3000} />
       
       </header>
     </div>
   );
+}
+App.propTypes = {
+  Images: PropTypes.array.isRequired,
+  Strings: PropTypes.array.strings.isRequired,
+  interval: PropTypes.number.isRequired
 }
 
 export default App;
--- imageChanger.jsx
+++ imageChanger.jsx
@@ -1,23 +1,15 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
-const Images = [
-    {   
-        
-        src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzFZN1C5pJ0WpFbrtY3CNhFvhyhiDEzlBRqw&usqp=CAU',
-        alt:'fuckYou'
-    },
-    {
-       
-        src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1yLWlhkn4jj3xufMaHayS7keZ0Ary2KfNmA&usqp=CAU',
-        alt:'fuckYou2'
-    },
-    {
-      
-        src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6y_fltB6Mn2NtTbdRcmphZg-h_TTDUZqrGA&usqp=CAU',
-        alt:'fuckYou3'
+
+
+
+const getNextString = (Strings, currentString) =>{
+    if (currentString === Strings.length - 1){
+        return 0;
     }
-]
-
+    return currentString +1;
+}
 const getNextIndex = (Images, currentIndex) =>{
     if (currentIndex === Images.length -1 ) {
         return 0;
@@ -31,33 +23,38 @@ class ImageChanger extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            Welcome :  ['Welcome to: '],
+            String :  [''],
             imageIndex:  0,
-           
+            stringIndex: 0
             
         };
-       
-        setInterval(() =>{
+       this.componentDidMount = (props) => {
+        this.intervalId = setInterval(() =>{
+            this.changeImage();
+        }, this.props.Interval)
+       }
+       this.componentWillUnmount = (props) => {
+        clearInterval(this.props.intervalId);
+       }
+       this.changeImage = (props) => {
+        this.setState((state, props)=> {
+            return{
+                imageIndex: getNextIndex(this.props.Images, state.imageIndex),
+                stringIndex: getNextString(this.props.Strings, state.stringIndex)
+            }
+        })
+       }
 
-            this.setState({Welcome: ['KaliMeNope.de']})
-                
-            this.setState({imageIndex: 
-                getNextIndex(Images, this.state.imageIndex)})
-            
-            
-        
-        }, 3000)
-        
 
     }
     render(){
-        const currentImage = Images[this.state.imageIndex];
-        
+        const currentImage = this.props.Images[this.state.imageIndex];
+        const currentString = this.props.Strings[this.state.stringIndex];
        
 
         return (
             <>
-            <h1>{this.state.Welcome}</h1>
+            <h1>{currentString['String']}</h1>
             <img imageIndex={this.state.imageIndex} 
             src={currentImage.src} alt={currentImage.alt} />
             </>
@@ -65,4 +62,12 @@ class ImageChanger extends React.Component {
     }
 
 }
+ImageChanger.propTypes = {
+ Images: PropTypes.array.isRequired,
+ imageIndex: PropTypes.number.isRequired,
+ Strings: PropTypes.array.Strings.isRequired,
+ String: PropTypes.string.isRequired,
+ stringIndex: PropTypes.number.isRequired
+};
+
 export default ImageChanger;
